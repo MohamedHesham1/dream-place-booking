b49f407b1c0742f8bb41d887d07058cc6972309e
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-12-02 08:48:08 +0200

    feat(router): :sparkles: expand router functionality
    - Added routes for 'Search Results' and 'Hotel Details' pages. - Implemented navigation guards to redirect users based on their login status and search parameters. Save to grepper

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../views/HomePage.vue';
-import RegisterPage from '../views/RegisterPage.vue';
-import LoginPage from '../views/LoginPage.vue';
 import { useUserStore } from '../stores/userStore';
+import HomePage from '../views/HomePage.vue';
+import LoginPage from '../views/LoginPage.vue';
+import RegisterPage from '../views/RegisterPage.vue';
+import SearchResultsPage from '../views/SearchResultsPage.vue';
+import HotelDetailsPage from '../views/HotelDetailsPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +24,16 @@ const router = createRouter({
       name: 'Register',
       component: RegisterPage,
     },
+    {
+      path: '/search-results',
+      name: 'Search Results',
+      component: SearchResultsPage,
+    },
+    {
+      path: '/hotel-details/:id/:checkInDate/:checkOutDate',
+      name: 'Hotel Details',
+      component: HotelDetailsPage,
+    },
   ],
 });
 
@@ -32,8 +44,16 @@ router.beforeEach((to, from, next) => {
     // If the user is logged in and tries to access the login or register page,
     // redirect them to the home page
     next({ name: 'Home' });
+  } else if (
+    to.name === 'Search Results' &&
+    Object.keys(to.query).length === 0
+  ) {
+    // If the user tries to access the search page without any search parameters,
+    // redirect them to the home page
+    next({ name: 'Home' });
   } else {
     next();
   }
 });
+
 export default router;
