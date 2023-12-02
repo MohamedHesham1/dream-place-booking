932fcac5612e340aa833f54634b59a730a5d4051
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-12-02 08:25:01 +0200

    feat(api): :sparkles: introduce hotel-related API calls

---
+++ src/api/apiCalls.js
@@ -0,0 +1,54 @@
+import fetchData from './fetchData';
+
+export const getSearchDestination = async (citiesDetails) => {
+  try {
+    const data = await fetchData('hotels/searchDestination', {
+      query: 'egypt',
+    });
+
+    citiesDetails.value = data.data.reduce((result, destination) => {
+      if (destination.dest_type === 'city') {
+        result.push({ name: destination.name, dest_id: destination.dest_id });
+      }
+      return result;
+    }, []);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getSearchHotels = async (sortOption, searchValues) => {
+  try {
+    const response = await fetchData('hotels/searchHotels', {
+      ...searchValues,
+      sort_by: sortOption,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getSortby = async (searchValues) => {
+  try {
+    const response = await fetchData('hotels/getSortBy', {
+      ...searchValues,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getHotelDetails = async (hotelId) => {
+  try {
+    const response = await fetchData('hotels/getHotelDetails', {
+      hotel_id: hotelId,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
